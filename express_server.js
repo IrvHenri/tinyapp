789224c3c88d29b404980c493b3aeb3656823eb1
Irving Henriquez <henriquezirv@gmail.com>
2021-05-10 19:33:26 +0000

    created urls_show.ejs to render single urls, created /urls/:shortURL route handler in rexpress_server.js

--- express_server.js
+++ express_server.js
@@ -20,6 +20,12 @@ app.get('/urls', (req,res)=>{
   res.render('urls_index',templateVars)
 })
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
